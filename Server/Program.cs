8db4db9252554d25eafecf916265ed903afe646c
Filip <hagiel120@gmail.com>
2023-05-30 16:11:49 +0200

    Qucik fix

--- Server/Program.cs
+++ Server/Program.cs
@@ -11,28 +11,19 @@ namespace Server
     public class Program
     {
         private UdpClient _udpConnectionServer;
-        private UdpClient _udpPaintServer;
         private List<IPEndPoint> _udpClients;
-        private BlockingCollection<KeyValuePair<byte, byte[]>> _paintCollection;
 
         private static void Main()
         {
             var program = new Program
             {
                 _udpConnectionServer = new UdpClient(7777),
-                _udpPaintServer = new UdpClient(7778),
                 _udpClients = new List<IPEndPoint>(),
-                _paintCollection = new BlockingCollection<KeyValuePair<byte, byte[]>>(
-                    new ConcurrentBag<KeyValuePair<byte, byte[]>>())
             };
 
             var watchingTask = Task.Factory.StartNew(() => program.WatchClients());
-            var collectPaintDataTask = Task.Factory.StartNew(() => program.CollectPaintData());
-            var sendPaintDataTask = Task.Factory.StartNew(() => program.SendPaintData());
 
             Console.WriteLine($"Server started at port {((IPEndPoint)program._udpConnectionServer.Client.LocalEndPoint).Port}");
-
-            Task.WaitAll(watchingTask, collectPaintDataTask, sendPaintDataTask);
         }
 
         private void WatchClients()
@@ -48,8 +39,7 @@ namespace Server
                     {
                         Console.WriteLine($"{endPoint} connected");
                         _udpClients.Add(endPoint);
-                        byte[] messageBack = BitConverter.GetBytes((short)((IPEndPoint)_udpPaintServer.Client.LocalEndPoint).Port);
-                        _udpConnectionServer.Send(messageBack, messageBack.Length, endPoint);
+                        //_udpConnectionServer.Send(messageBack, messageBack.Length, endPoint);
                     }
                     else if (message.Equals("disconnect"))
                     {
